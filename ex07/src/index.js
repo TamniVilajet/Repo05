0b454642d6dcb40f88c3f4d1649998d9fbf662bc
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-01 23:29:58 +0200

    Exercise 07

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    // only change code below this line
+    var myName = "Ermin Kirlic";
+    var myString = "My name is " + myName + " and I am well!";
+    // only change code above this line
+
+    return myString;
+}
+
+console.log(main());
+module.exports = main;
